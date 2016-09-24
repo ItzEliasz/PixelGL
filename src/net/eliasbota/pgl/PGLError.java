5ba33d0e0f550c089bfa5963b551947e79d88c73
Bota Elias <eliasbota85@gmail.com>
2016-09-24 11:44:15 +0300

    Added PGLError
    Added new class for errors.

---
+++ src/net/eliasbota/pgl/PGLError.java
@@ -0,0 +1,9 @@
+package net.eliasbota.pgl;
+
+public class PGLError {
+	
+	public static void throwContextError() {
+		System.err.println("PixelSurface has not been Initializated. Error: No Context");
+	}
+
+}
