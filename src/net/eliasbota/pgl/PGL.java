aeca8dc2b4c36c31651e811770683f424246b0d5
Bota Elias <eliasbota85@gmail.com>
2016-09-25 14:24:10 +0300

    Added Back Out-Of-Screen Pixels
    Now pixels can go again out of screen.

--- src/net/eliasbota/pgl/PGL.java
+++ src/net/eliasbota/pgl/PGL.java
@@ -1,3 +1,22 @@
+/**
+ * 
+ *  Copyright (C) 2016 Bota Elias & Dennis van Giessen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 package net.eliasbota.pgl;
 
 import net.eliasbota.pgl.memory.Memory;
@@ -30,7 +49,11 @@ public class PGL {
 	    }
 	    int numerator = longest >> 1 ;
 	    for (int i=0;i<=longest;i++) {
+	    	if(y > PixelSurface.getHeight() && x > PixelSurface.getWidth() || y > PixelSurface.getHeight() || x > PixelSurface.getWidth() || y < 0 && i < 0 || x < 0 || i < 0 || y > PixelSurface.getHeight() && x < 0 || x < 0 && y > PixelSurface.getHeight() || y > PixelSurface.getHeight() || x > PixelSurface.getWidth()) {
+	    		 						
+	        } else {
 	        PixelSurface.CoreFuncChangePixel(x, y, r, g, b);
+	        }
 	        numerator += shortest ;
 	        if (!(numerator<longest)) {
 	            numerator -= longest ;
@@ -42,5 +65,6 @@ public class PGL {
 	        }
 	    }
 	}
+	
 	
 }
